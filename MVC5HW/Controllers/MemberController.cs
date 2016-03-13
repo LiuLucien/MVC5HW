c2df23be6c0433a2ff1d2a15a17ad7b7f2b014f3
Lucien <lucien@miniasp.com>
2016-03-13 11:16:50 +0800

    增加表單驗證功能

---
+++ MVC5HW/Controllers/MemberController.cs
@@ -0,0 +1,55 @@
+﻿using MVC5HW.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVC5HW.Controllers
+{
+    [Authorize]
+    public class MemberController : Controller
+    {
+        // GET: Member
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost,AllowAnonymous]
+        public ActionResult Login(LoginViewModel login)
+        {
+            if (CheckLogin(login.Email, login.Password))
+            {
+                FormsAuthentication.RedirectFromLoginPage(login.Email, login.RememberMe);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("Password", "您輸入的帳號或密碼錯誤");
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private bool CheckLogin(string username, string password)
+        {
+            return (
+                username == "abc@gmail.com" &&
+                password == "123"
+                );
+        }
+    }
+}
